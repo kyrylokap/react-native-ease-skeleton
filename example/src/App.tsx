30f27047bd0c3fa15864921c5cebaf0b9603b5b9
kyrylokap <kapinos.kirill.99@gmail.com>
2026-03-19 19:03:13 +0100

    feat: introduce `Skeleton` component utilizing `react-native-ease` and update example app to demonstrate its usage.

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,20 @@
-import { Text, View, StyleSheet } from 'react-native';
-import { multiply } from 'react-native-ease-skeleton';
-
-const result = multiply(3, 7);
+import { View, StyleSheet, Text } from 'react-native';
+import { Skeleton } from 'react-native-ease-skeleton';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Skeleton
+        width={150}
+        height={50}
+        colorMode="light"
+        borderRadius={12}
+        show
+      />
+      <View style={styles.spacer} />
+      <Skeleton width={250} height={200} show colorMode="dark">
+        <Text style={styles.text}>Content</Text>
+      </Skeleton>
     </View>
   );
 }
@@ -16,5 +24,12 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'black',
+  },
+  spacer: {
+    height: 20,
+  },
+  text: {
+    color: 'white',
   },
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,85 @@
-export function multiply(a: number, b: number): number {
-  return a * b;
+import { EaseView } from 'react-native-ease';
+import {
+  View,
+  type DimensionValue,
+  type StyleProp,
+  type ViewStyle,
+} from 'react-native';
+import { StyleSheet } from 'react-native';
+const defaultColors = {
+  light: ['#E1E9EE', '#9e9e9eff'] as [string, string],
+  dark: ['#1D1D1D', '#333333'] as [string, string],
+};
+
+export interface SkeletonProps {
+  width?: DimensionValue;
+  height?: DimensionValue;
+  borderRadius?: number;
+  show: boolean;
+  colorMode?: 'light' | 'dark';
+  colors?: [string, string];
+  style?: StyleProp<ViewStyle>;
+  duration?: number;
+  children?: React.ReactNode;
 }
+/**
+ * Skeleton component for React Native
+ * @param width - Width of the skeleton
+ * @param height - Height of the skeleton
+ * @param borderRadius - Border radius of the skeleton
+ * @param show - Whether the skeleton should be shown
+ * @param colorMode - Color mode of the skeleton
+ * @param colors - Custom colors for the skeleton
+ * @param style - Custom styles for the skeleton
+ * @param duration - Duration of the animation
+ */
+export const Skeleton = ({
+  width = 100,
+  height = 100,
+  borderRadius = 4,
+  show,
+  colorMode = 'light',
+  colors,
+  style,
+  duration = 1000,
+  children,
+}: SkeletonProps) => {
+  const activeColors = colors || defaultColors[colorMode];
+  if (!show) {
+    return (
+      <View
+        style={[
+          { width, height, borderRadius },
+          style ? style : styles.container,
+        ]}
+      >
+        {children}
+      </View>
+    );
+  }
+  return (
+    <EaseView
+      style={[
+        { width, height, borderRadius },
+        style ? style : styles.container,
+      ]}
+      initialAnimate={{ backgroundColor: activeColors[0] }}
+      animate={{ backgroundColor: activeColors[1] }}
+      transition={{
+        type: 'timing',
+        duration: duration,
+        easing: 'easeInOut',
+        loop: 'reverse',
+      }}
+    >
+      {children}
+    </EaseView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
